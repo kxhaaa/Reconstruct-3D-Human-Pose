23149eac55e80da3c5949d1ce173ea7bbf2a63a0
Xianghan Kong <u7201650@anu.edu.au>
2021-10-29 16:16:12 +0800

    comments

--- predict_preprocess.py
+++ predict_preprocess.py
@@ -1,24 +1,77 @@
-import torch
-from torchvision.transforms import Normalize
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 20:38:29 2021
+
+@author: kong
+"""
 import numpy as np
 import cv2
-#import argparse
 import json
-
-#from models import hmr, SMPL
 from utils.imutils import crop
-# utils.renderer import Renderer
-#import config
+from torchvision.transforms import Normalize
 import constants
+import torch
 
 
+def bbox_from_openpose(openpose_file, rescale=1.2, detection_thresh=0.2):
+    """Get center and scale for bounding box from openpose detections."""
+    with open(openpose_file, 'r') as f:
+        keypoints = json.load(f)['people'][0]['pose_keypoints_2d']
+    keypoints = np.reshape(np.array(keypoints), (-1,3))
+    valid = keypoints[:,-1] > detection_thresh
+    valid_keypoints = keypoints[valid][:,:-1]
+    center = valid_keypoints.mean(axis=0)
+    bbox_size = (valid_keypoints.max(axis=0) - valid_keypoints.min(axis=0)).max()
+    # adjust bounding box tightness
+    scale = bbox_size / 200.0
+    scale *= rescale
+    return center, scale
 
 def bbox_from_json(bbox_file):
     """Get center and scale of bounding box from bounding box annotations.
     The expected format is [top_left(x), top_left(y), width, height].
     """
     with open(bbox_file, 'r') as f:
-        bboxs = np.array(json.load(f)['bbox']).astype(np.float32)
+        bbox = np.array(json.load(f)['bbox']).astype(np.float32)
+    ul_corner = bbox[:2]
+    center = ul_corner + 0.5 * bbox[2:]
+    width = max(bbox[2], bbox[3])
+    scale = width / 200.0
+    # make sure the bounding box is rectangular
+    return center, scale
+
+
+def process_image(img_file, bbox_file, openpose_file, input_res=224):
+    """Read image, do preprocessing and possibly crop it according to the bounding box.
+    If there are bounding box annotations, use them to crop the image.
+    If no bounding box is specified but openpose detections are available, use them to get the bounding box.
+    """
+    normalize_img = Normalize(mean=constants.IMG_NORM_MEAN, std=constants.IMG_NORM_STD)
+    img = cv2.imread(img_file)[:,:,::-1].copy() # PyTorch does not support negative stride at the moment
+    if bbox_file is None and openpose_file is None:
+        # Assume that the person is centerered in the image
+        height = img.shape[0]
+        width = img.shape[1]
+        center = np.array([width // 2, height // 2])
+        scale = max(height, width) / 200
+    else:
+        if bbox_file is not None:
+            center, scale = bbox_from_json(bbox_file)
+        elif openpose_file is not None:
+            center, scale = bbox_from_openpose(openpose_file)
+    img = crop(img, center, scale, (input_res, input_res))
+    img = img.astype(np.float32) / 255.
+    img = torch.from_numpy(img).permute(2,0,1)
+    norm_img = normalize_img(img.clone())[None]
+    return img, norm_img
+
+def bbox_from_json_multi(bbox_file):
+    """Get center and scale of bounding box from bounding box annotations.
+    The expected format is [top_left(x), top_left(y), width, height].
+    """
+    with open(bbox_file, 'r') as f:
+        bboxs = np.array(json.load(f)['bbox']).astype(int)
     centers = []
     scales = []
     for bbox in bboxs:
@@ -30,17 +83,18 @@ def bbox_from_json(bbox_file):
         scales.append(scale)
     # make sure the bounding box is rectangular
     return centers, scales
-#we hope this function will return several img correspond to different bounding box in one img
-def process_image(img_file, bbox_file, openpose_file, input_res=224):
+
+def process_image_multi(img_file, bbox_file, openpose_file, input_res=224):
     """Read image, do preprocessing and possibly crop it according to the bounding box.
     If there are bounding box annotations, use them to crop the image.
     If no bounding box is specified but openpose detections are available, use them to get the bounding box.
     """
     normalize_img = Normalize(mean=constants.IMG_NORM_MEAN, std=constants.IMG_NORM_STD)
     oimg = cv2.imread(img_file)[:,:,::-1].copy() # PyTorch does not support negative stride at the moment
-    centers, scales = bbox_from_json(bbox_file)
-    imgs = []
-    norm_imgs = []
+    centers, scales = bbox_from_json_multi(bbox_file)
+    
+    imgs = [oimg]
+    norm_imgs = [normalize_img(((torch.from_numpy(oimg.astype(np.float32)).permute(2,0,1))).clone())[None]]
     for i in range(len(centers)):
         img = crop(oimg, centers[i], scales[i], (input_res, input_res))
         img = img.astype(np.float32) / 255.
@@ -49,3 +103,5 @@ def process_image(img_file, bbox_file, openpose_file, input_res=224):
         imgs.append(img)
         norm_imgs.append(norm_img)
     return imgs, norm_imgs
+
+
